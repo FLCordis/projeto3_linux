e72c0e49230e26385d587fa751c9bd377cfac22e
Flavio Cordis <flavio.cordis@hotmail.com>
2022-08-23 09:15:37 -0300

    Added the Project Files

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1> Criação do Projeto para Bootcamp Linux Experience
+
+<h4> Repositório para o projeto final do curso.
+    
+</h4>
+
---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE infodata (
+    WorkerID int,
+    FirstName varchar(50),
+    SecondName varchar(50),
+    MainAddress varchar(150),
+    City varchar(50),
+    Host varchar(50)
+);
